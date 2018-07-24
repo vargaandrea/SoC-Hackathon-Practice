a50c9dc11570e33f30a34605b0267302c35fd527
vargaandrea <andi@narancs.net>
2018-07-24 14:23:30 +0200

    Add algorithm to find a word

--- week02_boggle/soc02h_boggle.py
+++ week02_boggle/soc02h_boggle.py
@@ -1,10 +1,11 @@
-
+import numpy
 
 test_mode = True
 
 words_dict = {}
 boggle_board = []
 
+directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, -1]]
 
 # ----------------------------------------------------- Main
 
@@ -26,27 +27,58 @@ def get_test_dictionary():
 
 def get_test_boggle_board():
     return [
-        ['Y', 'X', 'W', 'W', 'X'],
-        ['E', 'S', 'i', 'O', 'S'],
-        ['D', 'U', 'N', 'D', 'M'],
-        ['N', 'X', 'T', 'E', 'A'],
-        ['A', 'X', 'E', 'L', 'L'],
+        ['Y', 'X', 'C', 'R'],
+        ['E', 'S', 'I', 'E'],
+        ['D', 'M', 'U', 'X'],
+        ['N', 'D', 'N', 'E']
     ]
+    # return [
+    #     ['Y', 'X', 'W', 'W', 'X'],
+    #     ['E', 'S', 'i', 'O', 'S'],
+    #     ['D', 'U', 'N', 'D', 'M'],
+    #     ['N', 'X', 'T', 'E', 'A'],
+    #     ['A', 'X', 'E', 'L', 'L']
+    # ]
+
 
 
 def load_dictionary():
-    # TODO: load dictionary into  words_dict
-    return fill_test_words()
+    # TODO: load dictionary
     pass
 
 
 def generate_buggle_board():
     # TODO: generate board
-    return get_test_boggle_board()
     pass
 
 
+def is_word(word):
+    return word in words_dict.keys()
+
+
+def find_word(buggle_board, row, col, visited=[], found_words=[], w=''):
+    # print(row, col, ':')
+    if w == '':
+        visited = numpy.zeros([len(buggle_board), len(buggle_board)])
+
+    if 0 <= row < len(buggle_board) and 0 <= col < len(buggle_board) and not visited[row][col]:
+        new_word = w + buggle_board[row][col]
+        visited[row][col] = True
+
+        if is_word(new_word):
+            # save the word
+            # print('Word found: ', new_word)
+            found_words.append(new_word)
+
+        for dir in directions:
+                find_word(buggle_board, row + dir[0], col + dir[1], visited, words, new_word)
+
+        visited[row][col] = False
+
+    return found_words
+
 # ----------------------------------------------------- Main
+
 
 if test_mode:
     words_dict = get_test_dictionary()
@@ -59,5 +91,10 @@ else:
 
 # let's do the magic :)
 
-print(words_dict)
-print(boggle_board)
+words = []
+# for row in range(0, len(boggle_board)):
+#     for col in range(0, len(boggle_board)):
+
+words += find_word(boggle_board, 0, 0)
+words += find_word(boggle_board, 3, 3)
+print(words)
